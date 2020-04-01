45afbfdde9fbd0e901256cb1b3d8137390fb28bf
Nikes2121 <nikstuetze@gmail.com>
2020-04-01 11:41:29 -0700

    Add files via upload

---
+++ PlayerName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+    [SerializeField] private Image MyLabel;
+    [SerializeField] private Text MyName;
+    [SerializeField] private Transform position;
+   // Start is called before the first frame update
+
+
+   // Update is called once per frame
+   void Update()
+    {
+        MyLabel.transform.position = position.position;
+    }
+}
